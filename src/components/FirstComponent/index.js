e98c3282bb252474849021078bac0b5bba9b59fd
amit-getshogun <84618875+amit-getshogun@users.noreply.github.com>
2021-06-11 08:48:01 +0530

    changed styles.module to styles

--- src/components/FirstComponent/index.js
+++ src/components/FirstComponent/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import styles from './styles.module.css';
+import './styles.css';
 
 const FirstComponent = () => {
-  return <p className={styles.app}>First Component</p>;
+  return <p className='app'>First Component</p>;
 };
 
 export default FirstComponent;
