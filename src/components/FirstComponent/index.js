55db8d8fc12f4e405a96d3adffcfcfae018f8bf4
amit-getshogun <84618875+amit-getshogun@users.noreply.github.com>
2021-09-14 20:27:28 +0530

    testing component update

--- src/components/FirstComponent/index.js
+++ src/components/FirstComponent/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './styles.css';
 
 const FirstComponent = () => {
-  return <p className='app'>First Component</p>;
+  return <p className='app'>First Component1</p>;
 };
 
 export default FirstComponent;
